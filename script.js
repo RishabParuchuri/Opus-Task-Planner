d689e025fc82183869fba986d869c48303fc73b5
Rishab Paruchuri <rishparu@gmail.com>
2021-08-06 22:20:26 +0530

    UI Finished

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // alert("Script working")
+
+// const assignmentText = document.getElementById('assignment-text').value;
+// // var dateInput = document.querySelector('input[type="date"]');
+// // var submitButton = document.querySelector('#submit-button')
+
+// function test() {
+//     alert(assignmentText);
+//     // console.log(dateInput.value)
+// }
