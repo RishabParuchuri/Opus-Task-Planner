00d31607859ef5ea12c470d22f66e853e8861fed
Rishab Paruchuri <rishparu@gmail.com>
2021-08-21 12:45:22 +0530

    Added feature to add Homework objects to localStorage

--- script.js
+++ script.js
@@ -2,62 +2,52 @@
 var assignmentText = document.getElementById('assignmentText');
 var dateInput = document.querySelector('input[type="date"]');
 var submitButton = document.querySelector('#submitButton');
-var assigmentList = document.querySelector(".assignmentList");
+var assignmentList = document.querySelector(".assignmentList");
 
 submitButton.addEventListener('click', addAssignment)
-assigmentList.addEventListener('click', deleteFinish)
+assignmentList.addEventListener('click', deleteFinish)
 
 class Homework {
     constructor(title, date) {
-        this.title = title;
-        this.date = date;
-        this.completionStatus = false;
+      this.title = title;
+      this.date = date;
+      this.completionStatus = false;
     }
-}
+  }
 
 function addAssignment(event) {
     event.preventDefault();
-    // Stop page from refreshing an create div that contains the assignment
-    var {
-        assigmentDiv,
-        checkButton,
-        deleteButton,
-        assigmentTitle,
-        assigmentDate
-    } = createElements();
-    // append all the elements to their respective containers
-
-    addElements(assigmentDiv, checkButton, deleteButton, assigmentTitle, assigmentDate);
+// Stop page from refreshing an create div that contains the assignment
+    var { assignmentDiv, checkButton, deleteButton, assignmentTitle, assignmentDate } = createElements();
+// append all the elements to their respective containers
+    
+    addElements(assignmentDiv, checkButton, deleteButton, assignmentTitle, assignmentDate);
     // assignmentText.value = "";
     // dateInput.value = false;
 }
 
-function deleteFinish(e) {
+function deleteFinish(e){
     var item = e.target;
 
-    if (item.classList[0] === "trash-button") {
+    if(item.classList[0] === "trash-button"){
         item = deleteAnimation(item);
     }
 
-    if (item.classList[0] === "done-button") {
+    if(item.classList[0] === "done-button"){
         item = doneAnimation(item);
     }
 }
 
 function deleteAnimation(item) {
     item = item.parentElement;
-    playDeleteAudio();
-    item.classList.add('fall');
-    item.addEventListener("transitionend", function () {
-        item.remove();
-    })
-    return item;
-}
-
-function playDeleteAudio() {
     var audio = new Audio('swoosh.mp3');
     audio.play();
-    delete audio;
+    delete audio
+    item.classList.add('fall');
+    item.addEventListener("transitionend", function(){
+    item.remove();
+    })
+    return item;
 }
 
 function doneAnimation(item) {
@@ -65,30 +55,32 @@ function doneAnimation(item) {
     item.setAttribute("id", 'completed');
     item.children[2].style.textDecoration = "line-through";
     item.children[3].innerText = "COMPLETED";
-    playAudio();
+    var audio = new Audio('finish-sound.mp3');
+    audio.play();
+    delete audio
     item.classList.add('fallDone');
-    item.addEventListener("transitionend", function () {
+    item.addEventListener("transitionend", function(){
         item.classList.remove('fallDone');
-    })
+        })
     return item;
 }
 
-function playAudio() {
-    var audio = new Audio('finish-sound.mp3');
-    audio.play();
-    delete audio;
-}
-
 function createElements() {
-    var assigmentDiv = document.createElement("div");
-    assigmentDiv.classList.add("assignment");
-    // Create a list element with the assigment name
-    var assigmentTitle = document.createElement("li");
-    assigmentTitle.innerText = assignmentText.value;
+    var assignmentDiv = document.createElement("div");
+    assignmentDiv.classList.add("assignment");
+    // Create a list element with the assignment name
+    var assignmentTitle = document.createElement("li");
+    assignmentTitle.innerText = assignmentText.value;
+
     // Create a list element with the date 
-    var assigmentDate = document.createElement("li");
-    assigmentDate.classList.add("date");
-    assigmentDate.innerText = "Due: " + dateInput.value;
+    var assignmentDate = document.createElement("li");
+    assignmentDate.classList.add("date");
+    assignmentDate.innerText = "Due: " + dateInput.value;
+
+    // Add items to localStorage
+    let temp = new Homework(assignmentText.value, dateInput.value);
+    saveLocal(temp);
+    
     // create both the finish and delete buttons
     var checkButton = document.createElement("button");
     checkButton.innerHTML = "<i class='fas fa-check'></i>";
@@ -96,30 +88,24 @@ function createElements() {
     var deleteButton = document.createElement("button");
     deleteButton.innerHTML = "<i class='fas fa-times'></i>";
     deleteButton.classList.add("trash-button");
-    return {
-        assigmentDiv,
-        checkButton,
-        deleteButton,
-        assigmentTitle,
-        assigmentDate
-    };
+    return { assignmentDiv, checkButton, deleteButton, assignmentTitle, assignmentDate };
 }
 
-function addElements(assigmentDiv, checkButton, deleteButton, assigmentTitle, assigmentDate) {
-    assigmentDiv.appendChild(checkButton);
-    assigmentDiv.appendChild(deleteButton);
-    assigmentDiv.appendChild(assigmentTitle);
-    assigmentDiv.appendChild(assigmentDate);
-    assigmentList.appendChild(assigmentDiv);
+function addElements(assignmentDiv, checkButton, deleteButton, assignmentTitle, assignmentDate) {
+    assignmentDiv.appendChild(checkButton);
+    assignmentDiv.appendChild(deleteButton);
+    assignmentDiv.appendChild(assignmentTitle);
+    assignmentDiv.appendChild(assignmentDate);
+    assignmentList.appendChild(assignmentDiv);
 }
 
-function saveLocal(assigment){
-    let assigments;
+function saveLocal(assignment){
+    let assignments;
     if (localStorage.getItem("assignments") === null) {
-        assigments = [];
+        assignments = [];
     } else{
-        assigments = JSON.parse(localStorage.getItem("assigments"))
+        assignments = JSON.parse(localStorage.getItem("assignments"))
     }
-    assigments.push(assigment);
-    localStorage.setItem("assigments", JSON.stringify(assigments))
+    assignments.push(assignment);
+    localStorage.setItem("assignments", JSON.stringify(assignments));
 }
